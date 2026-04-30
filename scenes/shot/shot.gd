38d938814f5f233a694940149470354de1d1c7e4
IYuBelov <IYuBelov@gmail.com>
2026-05-01 00:28:55 +0300

    fix shot

--- scenes/shot/shot.gd
+++ scenes/shot/shot.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
 @export var speed: float = 600.0
+@onready var visibleNotifier = $VisibleOnScreenNotifier2D
+
+func _ready() -> void:
+	visibleNotifier.screen_exited.connect(self.on_visible_on_screen_notifier_2d_screen_exited)
 
 func _physics_process(delta: float) -> void:
 	position.x += speed * delta
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
+func on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
