0565b69b0b6648abdc8f0935e64d5518e1f16e1f
IYuBelov <IYuBelov@gmail.com>
2026-05-05 02:08:09 +0300

    refactoring

--- src/contexts/game_context/game_logic/game_area.gd
+++ src/contexts/game_context/game_logic/game_area.gd
@@ -1,16 +1,47 @@
+class_name GameArea
 extends Node
 
-class_name GameArea
-# Сигнал для UI, если зона вдруг изменится (например, при смене разрешения)
 signal boundary_changed(new_rect)
-
 
 @export var gameConfig: GameConfig
 
-# Текущая игровая зона
 var gameplay_area: Rect2
 
+@onready var lines = Lines.new(gameConfig.answer_lines_count, gameplay_area)
 
+func _ready() -> void:
+	get_tree().root.size_changed.connect(update_boundaries)
+	update_boundaries()
+
+func update_boundaries() -> void:
+	var screen_size = get_viewport().get_visible_rect().size
+	
+	# Оставляем отступы (например, 50 пикселей для UI)
+	var margin_top = 160.0
+	var margin_bottom = 20.0
+	var margin_side = 20.0
+	
+	gameplay_area = Rect2(
+		margin_side, 
+		margin_top, 
+		screen_size.x - (margin_side * 2), 
+		screen_size.y - margin_top - margin_bottom
+	)
+	lines.update(gameplay_area)
+	boundary_changed.emit(gameplay_area)
+
+
+func getLine(position:Vector2) -> int:
+	return lines.getLine(position)
+	
+	
+func getLinesSize() -> int:
+	return lines.getSize()
+	
+func getLines() -> Array[Rect2]:
+	return lines.getLines()
+	
+	
 class Lines extends RefCounted:
 	var lines: Array[Rect2] = []
 	
@@ -39,41 +70,3 @@ class Lines extends RefCounted:
 		
 	func getLines() -> Array[Rect2]:
 		return self.lines
-
-
-@onready var lines = Lines.new(gameConfig.answer_lines_count, gameplay_area)
-
-
-func _ready():
-	update_boundaries()
-	# Соединяем с сигналом изменения размера окна
-	get_tree().root.size_changed.connect(update_boundaries)
-
-func update_boundaries():
-	var screen_size = get_viewport().get_visible_rect().size
-	
-	# Оставляем отступы (например, 50 пикселей для UI)
-	var margin_top = 160.0
-	var margin_bottom = 20.0
-	var margin_side = 20.0
-	
-	gameplay_area = Rect2(
-		margin_side, 
-		margin_top, 
-		screen_size.x - (margin_side * 2), 
-		screen_size.y - margin_top - margin_bottom
-	)
-	lines.update(gameplay_area)
-	boundary_changed.emit(gameplay_area)
-
-
-func getLine(position:Vector2) -> int:
-	return lines.getLine(position)
-	
-	
-func getLinesSize() -> int:
-	return lines.getSize()
-	
-func getLines() -> Array[Rect2]:
-	return lines.getLines()
-	
