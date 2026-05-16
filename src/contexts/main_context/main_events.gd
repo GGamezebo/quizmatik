07d13adc87bafcb7e75dc55fb9f4a4685b34d692
Remzi13 <remiznik@hotmail.com>
2026-05-16 22:08:06 +0200

    Add post-battle context and update main context for scene transitions

--- src/contexts/main_context/main_events.gd
+++ src/contexts/main_context/main_events.gd
@@ -4,3 +4,4 @@ extends Resource
 @warning_ignore("unused_signal") signal ev_start_game
 @warning_ignore("unused_signal") signal ev_exit_game
 @warning_ignore("unused_signal") signal ev_reset_account_progress
+@warning_ignore("unused_signal") signal ev_return_to_menu
