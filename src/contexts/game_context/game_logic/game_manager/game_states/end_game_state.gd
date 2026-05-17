df6c6a2974319ccebc949b7cdfd16bcf685ed6ec
IYuBelov <IYuBelov@gmail.com>
2026-05-17 20:09:38 +0300

    dead animation only for looser

--- src/contexts/game_context/game_logic/game_manager/game_states/end_game_state.gd
+++ src/contexts/game_context/game_logic/game_manager/game_states/end_game_state.gd
@@ -5,11 +5,17 @@ static func get_state() -> String:
 	return FSMGameStates.END_GAME
 	
 @export var main_events: MainEvents
+@export var game_config: GameConfig
+@export var player: Player
 @export var air_plane: AirPlane
 	
 func enter(_prev_state: FSMState, _event_data: Dictionary) -> void:
 	air_plane.ev_dead_animation_finished.connect(_on_dead_animation_finished)
-	air_plane.die()
+	if player.score == game_config.questions_count:
+		# play win animation
+		main_events.ev_exit_game.emit()
+	else:
+		air_plane.die()
 	
 func leave(_event_data: Dictionary) -> void:
 	pass
