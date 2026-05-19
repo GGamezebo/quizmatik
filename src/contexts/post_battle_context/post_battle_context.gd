192cb71c779e57f56310b1c9dcc3a02c6654d9c1
Remzi13 <remiznik@hotmail.com>
2026-05-19 22:14:35 +0200

    Enhance post-battle context by adding main events and button connections; update scene structure and resource paths

--- src/contexts/post_battle_context/post_battle_context.gd
+++ src/contexts/post_battle_context/post_battle_context.gd
@@ -1,4 +1,15 @@
 extends IContext
 
+@export_category('Major components')
+@export var main_events: MainEvents
+@export_group('Buttons')
+@export var menu_button: Button
+@export var repeat_button: Button
+
 func _ready() -> void:
-	pass # Replace with function body.
+	repeat_button.grab_focus()
+	
+	menu_button.pressed.connect(_on_menu)
+
+func _on_menu() -> void:
+	main_events.ev_return_to_menu.emit()
