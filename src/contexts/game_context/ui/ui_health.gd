152215633b6bbca23897bcef38032a52e1f2555e
IYuBelov <iyubelov@gmail.com>
2026-06-07 00:30:07 +0200

    health icon

--- src/contexts/game_context/ui/ui_health.gd
+++ src/contexts/game_context/ui/ui_health.gd
@@ -6,33 +6,31 @@ extends HBoxContainer
 
 func _ready() -> void:
 	player.ev_health_changed.connect(_on_health_changed)
-	_updateState()
-	
+	_update_state()
+
 func _exit_tree() -> void:
 	player.ev_health_changed.disconnect(_on_health_changed)
 
-func _updateState() -> void:
+func _update_state() -> void:
 	var health: int = player.health
 	var child_count: int = get_child_count()
 	var count: int = health - child_count
-	var action = _createHealth if count > 0 else _removeHealth
+	var action := _create_health if count > 0 else _remove_health
 	for _index in range(abs(count)):
 		action.call()
-	
-func _on_health_changed(_health):
-	_updateState()
-	
-func _createHealth():
-	var new_texture_rect = TextureRect.new()
-	new_texture_rect.texture = helthSprite
-	new_texture_rect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-	new_texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-	new_texture_rect.custom_minimum_size = Vector2(64, 64)
-	add_child(new_texture_rect)
-	
-	
-func _removeHealth():
-	var child_count = get_child_count()
+
+func _on_health_changed(_health: int) -> void:
+	_update_state()
+
+func _create_health() -> void:
+	var icon := TextureRect.new()
+	icon.texture = helthSprite
+	icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	icon.custom_minimum_size = Vector2(52, 52)
+	add_child(icon)
+
+func _remove_health() -> void:
+	var child_count := get_child_count()
 	if child_count > 0:
-		var last_child = get_child(child_count - 1)
-		last_child.queue_free()
+		get_child(child_count - 1).queue_free()
