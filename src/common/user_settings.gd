7879cfc9f15796445efa685a6f2a72095b9ea895
IYuBelov <iyubelov@gmail.com>
2026-06-07 01:21:36 +0200

    turn off mouse key shoot event

--- src/common/user_settings.gd
+++ src/common/user_settings.gd
@@ -8,7 +8,7 @@ const SAVE_PATH: String = "user://settings.tres"
 
 @export_category("Controls")
 ## AirPlane.MovementMode: 0 = DIRECT, 1 = DISCRETE
-@export var movement_mode: int = 1
+@export var movement_mode: int = 0
 
 
 func save():
