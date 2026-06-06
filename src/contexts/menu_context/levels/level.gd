ae87d0a49463bfe06c559c3670c2d23de2158323
IYuBelov <iyubelov@gmail.com>
2026-06-07 00:08:48 +0200

    use stars fro levels

--- src/contexts/menu_context/levels/level.gd
+++ src/contexts/menu_context/levels/level.gd
@@ -1,5 +1,8 @@
 extends Button
 
+const STAR_FILLED_COLOR := Color(1.0, 0.84, 0.2, 1.0)
+const STAR_EMPTY_COLOR := Color(0.35, 0.42, 0.55, 0.45)
+
 @export var text_lable: Label
 @export var stars: Array[TextureRect]
 var is_exam: bool = false
@@ -12,11 +15,12 @@ func initialize(level_id: int, is_unlocked: bool, stars_count: int, _is_exam: bo
 func set_params(is_unlocked: bool, stars_count: int) -> void:
 	_set_params(is_unlocked, stars_count)
 	
-func _set_params(is_unlocked: bool, stars_count: int):
+func _set_params(is_unlocked: bool, stars_count: int) -> void:
 	for i in range(stars.size()):
-		if i < stars_count:
-			stars[i].visible = true
-		else:
+		if not is_unlocked:
 			stars[i].visible = false
-	
-	self.disabled = not is_exam and not is_unlocked
+			continue
+		stars[i].visible = true
+		stars[i].modulate = STAR_FILLED_COLOR if i < stars_count else STAR_EMPTY_COLOR
+
+	disabled = not is_exam and not is_unlocked
