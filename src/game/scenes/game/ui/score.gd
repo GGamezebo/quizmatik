3e015e85232aeac21ca796c88c5c0721b502242f
IYuBelov <iyubelov@gmail.com>
2026-07-22 00:52:45 +0200

    fix view scores

--- src/game/scenes/game/ui/score.gd
+++ src/game/scenes/game/ui/score.gd
@@ -6,10 +6,16 @@ extends HBoxContainer
 @export var maxScore: Label
 
 func _ready() -> void:
-	maxScore.text = str(game_config.questions_count)
+	_refresh_max_score()
+	game_config.changed.connect(_refresh_max_score)
 	score.text = str(player.score)
 	player.ev_score_changed.connect(_on_score_changed)
-	
+
+
+func _refresh_max_score() -> void:
+	maxScore.text = str(game_config.questions_count)
+
+
 func _on_score_changed(new_score: int) -> void:
 	score.text = str(new_score)
-	
+
