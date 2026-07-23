60dba72c0543cd664c46c2d248b2f072c5a4bb34
IYuBelov <iyubelov@gmail.com>
2026-07-24 00:07:42 +0200

    mute button refactoring

--- src/game/scenes/menu/music_mute_button.gd
+++ src/game/scenes/menu/music_mute_button.gd
@@ -1,27 +1,15 @@
-extends TextureButton
+extends MuteButton
 
 ## Toggle mute for the Music bus.
 
-@export var user_settings: UserSettings
 
-var _bus_index: int = -1
-
-
-func _ready() -> void:
-	_bus_index = AudioServer.get_bus_index("Music")
-	if _bus_index == -1:
-		push_error("Audio bus not found: Music")
-		return
-
-	toggle_mode = true
-	toggled.connect(_on_toggled)
-
-	var is_muted: bool = user_settings.is_music_mute
-	AudioServer.set_bus_mute(_bus_index, is_muted)
-	set_pressed_no_signal(is_muted)
+func _get_bus_name() -> String:
+	return "Music"
 
 
-func _on_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(_bus_index, toggled_on)
-	user_settings.is_music_mute = toggled_on
-	user_settings.save()
+func _is_muted() -> bool:
+	return user_settings.is_music_mute
+
+
+func _set_muted(is_mute: bool) -> void:
+	user_settings.is_music_mute = is_mute
---
+++ src/ui/buttons/mute_button.gd
@@ -0,0 +1,43 @@
+class_name MuteButton
+extends TextureButton
+
+## Base toggle that mutes an AudioServer bus and persists via UserSettings.
+
+@export var user_settings: UserSettings
+
+var _bus_index: int = -1
+
+
+func _ready() -> void:
+	var bus_name: String = _get_bus_name()
+	_bus_index = AudioServer.get_bus_index(bus_name)
+	if _bus_index == -1:
+		push_error("Audio bus not found: " + bus_name)
+		return
+
+	toggle_mode = true
+	toggled.connect(_on_toggled)
+
+	var is_muted: bool = _is_muted()
+	AudioServer.set_bus_mute(_bus_index, is_muted)
+	set_pressed_no_signal(is_muted)
+
+
+func _on_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(_bus_index, toggled_on)
+	_set_muted(toggled_on)
+	user_settings.save()
+
+
+func _get_bus_name() -> String:
+	push_error("MuteButton._get_bus_name() must be overridden")
+	return ""
+
+
+func _is_muted() -> bool:
+	push_error("MuteButton._is_muted() must be overridden")
+	return false
+
+
+func _set_muted(_is_mute: bool) -> void:
+	push_error("MuteButton._set_muted() must be overridden")
